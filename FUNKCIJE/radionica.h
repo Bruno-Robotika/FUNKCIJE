e100c6a9b6a56977b4b190fa69a917be3c578c35
Bruno-Robotika <128905099+Bruno-Robotika@users.noreply.github.com>
2023-06-17 12:09:52 +0200

    Update radionica.h

--- FUNKCIJE/radionica.h
+++ FUNKCIJE/radionica.h
@@ -2,10 +2,21 @@
 #include "helper.h"
 
 
-void RobotLine::radionica(){
-  
- Memory();
+void RobotLine::radionica() {
+  if (setup()) {
+    armOpen();
+  }
+  if (rightFront() > 200)
+    go(80, 20);
+  else
+    go(20, 80);
+
+  if (front() < 160) {
+    go(-90, 90);
+    delayMs(550);
+  }
+
+
 }
 
-  
-                                                       
+
